c49a1cc8f4fcfa950464a337b4884a22628938f3
gtvario <gtvario@yahoo.com>
2023-01-28 20:38:27 -0500

    Added text and radio button list

--- lib/widgets/charge_station_widget.dart
+++ lib/widgets/charge_station_widget.dart
@@ -8,8 +8,67 @@ class ChargeStation extends StatefulWidget {
 }
 
 class _ChargeStationState extends State<ChargeStation> {
+  String? chargeStationState;
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      children: <Widget>[
+        const Align(
+          alignment: Alignment.centerLeft,
+          child: Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Text(
+              "Charge Station",
+              style: TextStyle(fontSize: 20),
+            ),
+          ),
+        ),
+        RadioListTile(
+            title: const Text('Engaged'),
+            value: 'Engaged',
+            groupValue: chargeStationState,
+            onChanged: (value) {
+              setState(() {
+                chargeStationState = value.toString();
+              });
+            }),
+        RadioListTile(
+            title: const Text('Docked'),
+            value: 'Docked',
+            groupValue: chargeStationState,
+            onChanged: (value) {
+              setState(() {
+                chargeStationState = value.toString();
+              });
+            }),
+        RadioListTile(
+            title: const Text('Parked'),
+            value: 'Parked',
+            groupValue: chargeStationState,
+            onChanged: (value) {
+              setState(() {
+                chargeStationState = value.toString();
+              });
+            }),
+        RadioListTile(
+            title: const Text('Attempted'),
+            value: 'Attempted',
+            groupValue: chargeStationState,
+            onChanged: (value) {
+              setState(() {
+                chargeStationState = value.toString();
+              });
+            }),
+        RadioListTile(
+            title: const Text('Did Not Attempt'),
+            value: 'Did Not Attempt',
+            groupValue: chargeStationState,
+            onChanged: (value) {
+              setState(() {
+                chargeStationState = value.toString();
+              });
+            }),
+      ],
+    );
   }
 }
