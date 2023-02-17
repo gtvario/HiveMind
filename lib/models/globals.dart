f61b6b049e80d4e3420b05006aa96321874c0eb9
gtvario <gtvario@yahoo.com>
2023-02-17 00:29:47 -0500

    Password to unlock drawer tiles

---
+++ lib/models/globals.dart
@@ -0,0 +1,1 @@
+int queenPassword = 8675309;
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:hivemind/models/globals.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  bool isEnabled = false;
 
   @override
   Widget build(BuildContext context) {
@@ -12,7 +20,20 @@ class HomePage extends StatelessWidget {
           Padding(
             padding: const EdgeInsets.only(right: 40.0),
             child: GestureDetector(
-              onTap: () {},
+              onTap: () async {
+                _passwordInputController.clear();
+                var input = await _showPasswordInputDialog(context);
+                int? password = int.tryParse(input ?? "");
+                if (password == queenPassword) {
+                  setState(() {
+                    isEnabled = true;
+                  });
+                } else if (password != queenPassword && password != null) {
+                  setState(() {
+                    isEnabled = false;
+                  });
+                }
+              },
               child: const Icon(
                 Icons.lock,
                 size: 26.0,
@@ -23,17 +44,48 @@ class HomePage extends StatelessWidget {
       ),
       drawer: Drawer(
         child: ListView(
-          children: const [
-            DrawerHeader(
+          children: [
+            const DrawerHeader(
               child: null,
             ),
             ListTile(
-              leading: Icon(Icons.person_add),
-              title: Text("Add/Edit Students"),
-            ),
+                leading: const Icon(Icons.person_add),
+                title: const Text("Add/Edit Students"),
+                enabled: isEnabled,
+                onTap: () {
+                  Navigator.pop(context);
+                }),
           ],
         ),
       ),
     );
+  }
+
+  final _passwordInputController = TextEditingController();
+
+  Future<String?> _showPasswordInputDialog(BuildContext context) async {
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: const Text('Enter Passcode'),
+            content: TextField(
+              controller: _passwordInputController,
+              decoration:
+                  const InputDecoration(hintText: "Jenny I've Got Your Number"),
+            ),
+            actions: <Widget>[
+              ElevatedButton(
+                child: const Text("Cancel"),
+                onPressed: () => Navigator.pop(context),
+              ),
+              ElevatedButton(
+                child: const Text('OK'),
+                onPressed: () =>
+                    Navigator.pop(context, _passwordInputController.text),
+              ),
+            ],
+          );
+        });
   }
 }
