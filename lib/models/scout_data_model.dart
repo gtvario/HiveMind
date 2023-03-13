e2642b468819e5c82775c1859f961bcee08d3911
Chris Wallace <cmwallace1102@yahoo.com>
2023-03-13 18:02:43 -0400

    Updated Scout Data Model

--- lib/models/scout_data_model.dart
+++ lib/models/scout_data_model.dart
@@ -34,7 +34,8 @@ class ScoutData {
       hpStationAuto = 0,
       fieldConeAuto = 0,
       fieldCubeAuto = 0,
-      droppedAuto = 0;
+      droppedAutoCone = 0,
+      droppedAutoCube = 0;
 
   // Teleop
   List<int> scoreGridTeleop = [
@@ -70,11 +71,15 @@ class ScoutData {
   int hpStationTeleop = 0,
       fieldConeTeleop = 0,
       fieldCubeTeleop = 0,
-      droppedTeleop = 0;
+      droppedTeleopCone = 0,
+      droppedTeleopCube = 0;
 
   // Endgame
-  String endgameChargeStation = "Did Not Attempt", comments = "";
+  String endgameChargeStation = "Did Not Attempt",
+      robotCondition = "",
+      comments = "";
   int defenseScore = 3, drivingScore = 3;
+  bool playedDefense = false;
 
   ScoutData({
     required this.autoChargeStation,
