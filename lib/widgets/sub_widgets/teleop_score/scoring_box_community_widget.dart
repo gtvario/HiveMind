de6bc58090b4d3cc45e28a3ca52c2ae244d95f1e
Chris Wallace <cmwallace1102@yahoo.com>
2023-04-17 20:26:00 -0400

    Reverted Hybrid Box Changes For SuperCharging

--- lib/widgets/sub_widgets/teleop_score/scoring_box_community_widget.dart
+++ lib/widgets/sub_widgets/teleop_score/scoring_box_community_widget.dart
@@ -19,7 +19,6 @@ class ScoringBoxCommunityWidget extends StatefulWidget {
 
 class _ScoringBoxCommunityWidgetState extends State<ScoringBoxCommunityWidget> {
   Color _color = Colors.grey;
-  String _text = '';
 
   @override
   void initState() {
@@ -35,24 +34,12 @@ class _ScoringBoxCommunityWidgetState extends State<ScoringBoxCommunityWidget> {
     }
 
     setState(() {
-      // 0 = Empty
-      // 1 = Cube
-      // 2 = Cone
-      // 3 = Cube x2
-      // 4 = Cone x2
-      // 5 = Hybrid x2
       if (correspondingBoxVal == 1) {
         _color = Colors.purple;
-        _text = '';
       } else if (correspondingBoxVal == 2) {
         _color = Colors.yellow;
-        _text = '';
-      } else if (correspondingBoxVal == 5) {
-        _color = Colors.lightBlueAccent;
-        _text = 'x2';
       } else {
         _color = Colors.grey;
-        _text = '';
       }
     });
   }
@@ -63,32 +50,25 @@ class _ScoringBoxCommunityWidgetState extends State<ScoringBoxCommunityWidget> {
       body: GestureDetector(
         onTap: () {
           setState(() {
-            if (widget.gameMode == "auton") {
-              _text = '';
-              if (_color == Colors.yellow) {
-                _color = Colors.purple;
+            if (_color == Colors.yellow) {
+              _color = Colors.purple;
+              if (widget.gameMode == "auton") {
                 widget.scoutData.setAutoGrid = [widget.boxIndex, 1];
-              } else if (_color == Colors.purple) {
-                _color = Colors.grey;
+              } else {
+                widget.scoutData.setTeleopGrid = [widget.boxIndex, 1];
+              }
+            } else if (_color == Colors.purple) {
+              _color = Colors.grey;
+              if (widget.gameMode == "auton") {
                 widget.scoutData.setAutoGrid = [widget.boxIndex, 0];
               } else {
-                _color = Colors.yellow;
-                widget.scoutData.setAutoGrid = [widget.boxIndex, 2];
+                widget.scoutData.setTeleopGrid = [widget.boxIndex, 0];
               }
             } else {
-              _text = '';
-              if (_color == Colors.yellow) {
-                _color = Colors.purple;
-                widget.scoutData.setTeleopGrid = [widget.boxIndex, 1];
-              } else if (_color == Colors.purple) {
-                _color = Colors.lightBlueAccent;
-                _text = 'x2';
-                widget.scoutData.setTeleopGrid = [widget.boxIndex, 5];
-              } else if (_color == Colors.lightBlueAccent) {
-                _color = Colors.grey;
-                widget.scoutData.setTeleopGrid = [widget.boxIndex, 0];
+              _color = Colors.yellow;
+              if (widget.gameMode == "auton") {
+                widget.scoutData.setAutoGrid = [widget.boxIndex, 2];
               } else {
-                _color = Colors.yellow;
                 widget.scoutData.setTeleopGrid = [widget.boxIndex, 2];
               }
             }
@@ -99,13 +79,6 @@ class _ScoringBoxCommunityWidgetState extends State<ScoringBoxCommunityWidget> {
             padding: const EdgeInsets.all(3.0),
             decoration: BoxDecoration(
                 border: Border.all(color: Colors.black), color: _color),
-            child: Center(
-              child: Text(_text,
-                  style: const TextStyle(
-                      fontSize: 20.0,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black)),
-            ),
           ),
         ),
       ),
