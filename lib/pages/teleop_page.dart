2658dc0bfe57afa676d6faae0042f6193c594565
gtvario <gtvario@yahoo.com>
2024-01-30 21:43:37 -0500

    Added field and counter boxes

--- lib/pages/teleop_page.dart
+++ lib/pages/teleop_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hivemind/models/scout_data_model.dart';
+import 'package:hivemind/widgets/counter_box_widget.dart';
 import 'package:hivemind/widgets/teleop_scoring_widget.dart';
 
 class TeleopPage extends StatefulWidget {
@@ -11,6 +12,63 @@ class TeleopPage extends StatefulWidget {
 }
 
 class _TeleopPageState extends State<TeleopPage> {
+  OverlayEntry? overlayEntry;
+  int currentPageIndex = 0;
+  var _tapPosition;
+  void _handleTapDown(TapDownDetails details) {
+    final RenderBox referenceBox = context.findRenderObject() as RenderBox;
+    setState(() {
+      _tapPosition = referenceBox.globalToLocal(details.globalPosition);
+    });
+  }
+
+  void createHighlightOverlay({
+    required AlignmentDirectional alignment,
+    required Color borderColor,
+    required double xPos,
+    required double yPos,
+  }) {
+    // Remove the existing OverlayEntry.
+    double adjustedXPos = ((xPos - 10) - 470) / 470;
+    double adjustedYPos = ((yPos + 35) - 250) / 250;
+
+    removeHighlightOverlay();
+
+    assert(overlayEntry == null);
+
+    overlayEntry = OverlayEntry(
+      // Create a new OverlayEntry.
+      builder: (BuildContext context) {
+        // Align is used to position the highlight overlay
+        // relative to the NavigationBar destination.
+        return SafeArea(
+          child: Align(
+            alignment: Alignment(adjustedXPos, adjustedYPos),
+            heightFactor: 1.0,
+            child: Transform.scale(
+                scale: 1.5, child: const Icon(Icons.circle, color: Colors.red)),
+          ),
+        );
+      },
+    );
+
+    // Add the OverlayEntry to the Overlay.
+    Overlay.of(context, debugRequiredFor: widget).insert(overlayEntry!);
+  }
+
+  // Remove the OverlayEntry.
+  void removeHighlightOverlay() {
+    overlayEntry?.remove();
+    overlayEntry = null;
+  }
+
+  @override
+  void dispose() {
+    // Make sure to remove OverlayEntry when the widget is disposed.
+    removeHighlightOverlay();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -25,16 +83,37 @@ class _TeleopPageState extends State<TeleopPage> {
         ),
         const SizedBox(height: 10),
         Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          mainAxisSize: MainAxisSize.max,
+          //Scoring counters
           children: [
-            SizedBox(width: 30),
-            TeleopScoringWidget(
-              mode: 'teleop',
-              scoutData: widget.scoutData,
-            ),
+            CounterBox(),
+            CounterBox(),
+            CounterBox(),
           ],
         ),
+        Row(
+          children: [
+            Container(
+              width: 800,
+              height: 250,
+              child: GestureDetector(
+                //Add onTap
+                child: Image.asset("assets/images/field24.png"),
+                onTapDown: (details) {
+                  _handleTapDown(details);
+                  setState(() {
+                    currentPageIndex = 1;
+                  });
+                  createHighlightOverlay(
+                    alignment: AlignmentDirectional.bottomStart,
+                    borderColor: Colors.red,
+                    xPos: _tapPosition.dx,
+                    yPos: _tapPosition.dy,
+                  );
+                },
+              ),
+            ),
+          ],
+        )
       ],
     );
   }
